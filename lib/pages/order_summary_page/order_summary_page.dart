b118f2b585a3dfea1b24f3d7977221d1ecffc471
eeny3 <xabuovx@gmail.com>
2023-10-23 12:36:42 +0600

    completed page

--- lib/pages/order_summary_page/order_summary_page.dart
+++ lib/pages/order_summary_page/order_summary_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:fitb_pantry_app/models/group_model/group_model.dart';
 import 'package:fitb_pantry_app/pages/order_summary_page/order_summary_page_store.dart';
+import 'package:fitb_pantry_app/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -28,118 +29,156 @@ class _OrderSummaryPageState extends State<OrderSummaryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Observer(
-        builder: (context) {
-          if(pageStore.isLoading) {
-            if (pageStore.isLoading) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: Colors.black,
-                ),
-              );
-            }
+      body: Observer(builder: (context) {
+        if (pageStore.isLoading) {
+          if (pageStore.isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.black,
+              ),
+            );
           }
-          return Padding(
-            padding: EdgeInsets.fromLTRB(16.w, 32.h, 16.w, 0),
-            child: ListView.builder(
-              itemCount: pageStore.orderSummaryList.length + 1,
-              itemBuilder: (context, index) {
-                if (index == 0) {
-                  return Padding(
-                    padding: EdgeInsets.only(
-                      bottom: 48.h,
-                    ),
-                    child: Text(
-                      'Your order summary',
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 32.sp,
-                      ),
-                    ),
-                  );
-                }
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    SizedBox(
-                      height: 8.h,
-                    ),
-                    Text(
-                      '$index. ${pageStore.orderSummaryList[index - 1].name}',
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        fontSize: 18.sp,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 16.h,
-                    ),
-                    ListView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: pageStore.orderSummaryList[index - 1].items.length,
-                        itemBuilder: (BuildContext context, itemIndex) {
-                          return Padding(
-                            padding: EdgeInsets.symmetric(vertical: 8.h),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: const Color(0xff4AC074),
-                                borderRadius: BorderRadius.circular(10.r),
-                                border: Border.all(
-                                  color: const Color(0xff4AC074),
-                                  width: 4.r,
-                                ),
-                              ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Container(
-                                    width: 65.w,
-                                    height: 50.h,
-                                    clipBehavior: Clip.hardEdge,
-                                    decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(10.r),
-                                        bottomLeft: Radius.circular(10.r),
-                                      ),
-                                    ),
-                                    child: Image.network(
-                                      pageStore.orderSummaryList[index - 1]
-                                          .items[itemIndex]
-                                          .image,
-                                      fit: BoxFit.contain,
-                                    ),
-                                  ),
-                                  Text(
-                                    pageStore.orderSummaryList[index - 1]
-                                        .items[itemIndex]
-                                        .label,
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w700,
-                                      fontSize: 18.sp,
-                                    ),
-                                  ),
-                                  SizedBox(),
-                                ],
-                              ),
-                            ),
-                          );
-                        }),
-                    SizedBox(
-                      height: 8.h,
-                    ),
-                  ],
-                );
-              },
-            ),
-          );
         }
-      ),
+        return Padding(
+          padding: EdgeInsets.fromLTRB(16.w, 32.h, 16.w, 0),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: pageStore.orderSummaryList.length + 1,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return Padding(
+                        padding: EdgeInsets.only(
+                          bottom: 48.h,
+                        ),
+                        child: Text(
+                          'Your order summary',
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 32.sp,
+                          ),
+                        ),
+                      );
+                    }
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SizedBox(
+                          height: 8.h,
+                        ),
+                        pageStore.orderSummaryList[index - 1].items.isNotEmpty
+                            ? Text(
+                                '$index. ${pageStore.orderSummaryList[index - 1].name}',
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontSize: 18.sp,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              )
+                            : const SizedBox(),
+                        SizedBox(
+                          height: 16.h,
+                        ),
+                        ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount:
+                                pageStore.orderSummaryList[index - 1].items.length,
+                            itemBuilder: (BuildContext context, itemIndex) {
+                              return Padding(
+                                padding: EdgeInsets.symmetric(vertical: 8.h),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xff4AC074),
+                                    borderRadius: BorderRadius.circular(10.r),
+                                    border: Border.all(
+                                      color: const Color(0xff4AC074),
+                                      width: 4.r,
+                                    ),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        width: 65.w,
+                                        height: 50.h,
+                                        clipBehavior: Clip.hardEdge,
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(10.r),
+                                            bottomLeft: Radius.circular(10.r),
+                                          ),
+                                        ),
+                                        child: Image.network(
+                                          pageStore.orderSummaryList[index - 1]
+                                              .items[itemIndex].image,
+                                          fit: BoxFit.contain,
+                                        ),
+                                      ),
+                                      Text(
+                                        pageStore.orderSummaryList[index - 1]
+                                            .items[itemIndex].label,
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 18.sp,
+                                        ),
+                                      ),
+                                      const SizedBox(),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            }),
+                        SizedBox(
+                          height: 8.h,
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 32),
+                child: ElevatedButton(
+                  onPressed: () {
+                    context.router.replace(const CompletedRoute());
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shadowColor: Colors.transparent,
+                    elevation: 0.0,
+                  ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 18.w,
+                      vertical: 20.h,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color(0xff2F4DED),
+                      borderRadius: BorderRadius.circular(16.r),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Done',
+                        style: TextStyle(
+                          fontSize: 32.sp,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:fitb_pantry_app/pages/startup_page/startup_page.dart';
 import 'package:fitb_pantry_app/pages/login_page/login_page.dart';
 import 'package:fitb_pantry_app/pages/order_page/order_page.dart';
+import 'package:fitb_pantry_app/pages/completed_page.dart';
 import 'package:fitb_pantry_app/pages/order_summary_page/order_summary_page.dart';
 import 'package:fitb_pantry_app/models/group_model/group_model.dart';
 import 'package:flutter/material.dart';
@@ -24,6 +25,9 @@ class AppRouter extends _$AppRouter {
         ),
         AutoRoute(
           page: OrderSummaryRoute.page,
+        ),
+        AutoRoute(
+          page: CompletedRoute.page,
         ),
       ];
 }
